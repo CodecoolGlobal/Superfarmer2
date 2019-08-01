c3e41afffb55e84a93c7638213c8357192079eb6
Andrea Juhasz <jhszandrea@gmail.com>
2019-08-01 11:38:21 +0200

    showValues in main function, id name changes in bank bar

--- static/js/game.js
+++ static/js/game.js
@@ -55,6 +55,7 @@ const main = function() {
     var bank = {'rabbit': 60, 'sheep': 14, 'pig': 20, 'cow': 12, 'horse': 6, 'small_dog': 4, 'big_dog': 2};
     var user1 = {'rabbit': 0, 'sheep': 0, 'pig': 0, 'cow': 0, 'horse': 0, 'small_dog': 0, 'big_dog': 0};
     var user2 = {'rabbit': 0, 'sheep': 0, 'pig': 0, 'cow': 0, 'horse': 0, 'small_dog': 0, 'big_dog': 0};
+    showValues(bank, user1, user2);
     buttonEvents(bank, user1, user2);
 };
 
